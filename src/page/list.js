96fa6b7fd9aa3b9a171f815b333cd92fac1e1c95
minjeong-dev <vitaminjeong1026@gmail.com>
2022-07-17 18:43:25 +0900

    응답받은 데이터 맵핑 후 페이지 구현

--- src/page/list.js
+++ src/page/list.js
@@ -18,8 +18,27 @@ function List() {
     });
     setData(data_list);
   };
-  console.log(data);
-  return <div></div>;
+  const list = data.data;
+  return (
+    <div className='list'>
+      <div className='list-title list-box'>
+        <div>제목</div>
+        <div>조회수</div>
+        <div className='acenter'>날짜</div>
+      </div>
+      {list
+        ? list.map((el, key) => {
+            return (
+              <div className='list-data list-box' key={key}>
+                <div>{el.title}</div>
+                <div></div>
+                <div className='acenter'>{el.date.slice(0, 10)}</div>
+              </div>
+            );
+          })
+        : null}
+    </div>
+  );
 }
 
 export default List;
