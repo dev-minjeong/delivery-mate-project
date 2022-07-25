ba915d34952bbefd8aaa5002f364b2701748ecba
minjeong-dev <vitaminjeong1026@gmail.com>
2022-07-25 19:25:43 +0900

    게시판 리스트 상세페이지 데이터 조회

--- src/page/view.js
+++ src/page/view.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
 
 import './main.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const View = () => {
   const board_data = useParams();
+  const [data, setData] = useState([]);
+  const [date, setDate] = useState('');
 
   useEffect(() => {
     gettingData();
   }, []);
 
+  // 리스트 데이터 조회
   const gettingData = async () => {
     const board_id = board_data.data;
     const getData = await axios('/get/board_data', {
@@ -18,8 +21,42 @@ const View = () => {
       headers: new Headers(),
       data: { id: board_id },
     });
-    console.log(getData);
+
+    // 날짜 구하기
+    const date =
+      getData.data.data[0].date.slice(0, 10) +
+      ' ' +
+      getData.data.data[0].date.slice(11, 16);
+    setData(getData.data);
+    setDate(date);
+    // return { data: setData(getData), date: setDate(date) };
   };
-  return <div>View Page</div>;
+
+  return (
+    <div className='write'>
+      {data.data ? (
+        <div>
+          <div className='top-title'>
+            <input
+              type='text'
+              id='title-txt'
+              name='title'
+              defaultValue={data.data[0].title}
+              readOnly
+            ></input>
+            <div className='date-box'>{date}</div>
+          </div>
+          <div>
+            <textarea
+              id='content-txt'
+              name='contents'
+              defaultValue={data.data[0].contents}
+              readOnly
+            ></textarea>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 export default View;
