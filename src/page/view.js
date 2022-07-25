9a8d258a56b61cf814d46cc77382356822686dc3
minjeong-dev <vitaminjeong1026@gmail.com>
2022-07-25 17:43:45 +0900

    useParams를 활용해 데이터 조회(에러 해결)

--- src/page/view.js
+++ src/page/view.js
@@ -2,14 +2,17 @@ import axios from 'axios';
 
 import './main.css';
 import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-const View = (props) => {
+const View = () => {
+  const board_data = useParams();
+
   useEffect(() => {
     gettingData();
   }, []);
 
   const gettingData = async () => {
-    const board_id = props;
+    const board_id = board_data.data;
     const getData = await axios('/get/board_data', {
       method: 'POST',
       headers: new Headers(),
