f8f6d69946fd0f125a8c7bdb4bfba5a8c8891cd8
minjeong-dev <vitaminjeong1026@gmail.com>
2022-08-21 22:18:09 +0900

    비밀번호 찾기 폼 구성

--- src/inc/search_pw.js
+++ src/inc/search_pw.js
@@ -20,6 +20,29 @@ function SearchPw({
           backSearchModal={backSearchModal}
           target={target}
         ></BackAndClose>
+        <div className='search-box'>
+          <h4>비밀번호 찾기</h4>
+          <div>
+            <h5>아이디</h5>
+            <input type='text' maxLength='15' name='search-pw-id'></input>
+          </div>
+          <div>
+            <h5>이메일</h5>
+            <input type='text' maxLength='20' name='search-pw-email'></input>@
+            <input
+              type='text'
+              maxLength='15'
+              name='search-pw-write-email'
+            ></input>
+          </div>
+          <div>
+            <input
+              type='button'
+              value='조회하기'
+              name='search-pw-submit'
+            ></input>
+          </div>
+        </div>
       </Modal>
     </>
   );
