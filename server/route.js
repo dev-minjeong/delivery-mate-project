eba75283d3c06b9223102ebb046686545c6437bf
minjeong-dev <vitaminjeong1026@gmail.com>
2022-08-27 23:37:04 +0900

    좋아요, 보드 업데이트, 삭제 라우터 추가

--- server/route.js
+++ server/route.js
@@ -14,15 +14,18 @@ router.post('/add/user', controller.add.user);
 router.post('/update/view_cnt', controller.update.view_cnt);
 router.post('/update/pw', controller.update.pw);
 router.post('/update/like', controller.update.like);
+router.post('/update/board', controller.update.board);
 
 router.post('/delete/category', controller.delete.category);
+router.post('/delete/board', controller.delete.board);
 
 router.post('/modify/category', controller.modify.category);
 
 router.get('/get/category', controller.get.category);
-
 router.post('/get/board', controller.get.board);
 router.post('/get/board_cnt', controller.get.board_cnt);
 router.post('/get/board_data', controller.get.board_data);
+
+router.post('/check/like', controller.check.like);
 
 module.exports = router;
